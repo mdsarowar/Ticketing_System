d4309c97aea99163811649983ab0c0aa0a3a5fd3
mdsarowar <sarowara8@gmail.com>
2025-10-22 18:38:09 +0600

    Project setup with user authentication

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Admin user
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@test.com',
+            'password' => Hash::make('password'),
+            'role' => 'admin',
+        ]);
+
+        // Customer user
+        User::create([
+            'name' => 'Customer',
+            'email' => 'customer@test.com',
+            'password' => Hash::make('password'),
+            'role' => 'customer',
+        ]);
+    }
+}
