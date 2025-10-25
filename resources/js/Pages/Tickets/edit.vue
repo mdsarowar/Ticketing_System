887a33a77f8848d2b612de0f9ba0faf222171d1e
mdsarowar <sarowara8@gmail.com>
2025-10-25 08:44:34 +0600

    a error solve

--- resources/js/Pages/Tickets/edit.vue
+++ resources/js/Pages/Tickets/edit.vue
@@ -198,7 +198,7 @@
 <script setup>
 import { ref } from 'vue'
 import { useForm, Link } from '@inertiajs/vue3'
-import AppLayout from '@/Layouts/AppLayout.vue'
+import AppLayout from '../../layouts/AppLayout.vue'
 
 const props = defineProps({
     ticket: {
