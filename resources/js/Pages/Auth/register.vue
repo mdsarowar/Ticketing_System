019e749a3233ad0e457979f56c2e1f4a755dc18b
mdsarowar <sarowara8@gmail.com>
2025-10-25 08:46:50 +0600

    layouts path correction

--- resources/js/Pages/Auth/register.vue
+++ resources/js/Pages/Auth/register.vue
@@ -122,7 +122,7 @@
 
 <script setup>
 import { useForm, Link } from '@inertiajs/vue3'
-import GuestLayout from '@/Layouts/GuestLayout.vue'
+import GuestLayout from '../../Layouts/GuestLayout.vue'
 
 const form = useForm({
     name: '',
